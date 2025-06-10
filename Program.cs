6e7b81d5497b169841a1c521d9d7dc97cbe3c720
leon.schneider <leonschneider29@googlemail.com>
2025-06-10 11:21:40 +0200

    Read in File

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace C__Konsolenapp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿namespace C__Konsolenapp {
+    internal class Program {
+        static void Main(string[] args) {
+            List<String[]> artikel = new();
+            string file = args[0].ToString();
+            //string file = "S:\\Leon-Schneider\\repos\\C#-Konsolenapp\\artikel_neu.csv";
+            if (File.Exists(file)) {
+                String[] lines = File.ReadAllLines(file);
+                for (int i = 0; i < lines.Length; i++) {
+                    if (i == 1) continue;
+                    string line = lines[i];
+                    //Console.WriteLine(line);
+                    artikel.Add(line.Split(";"));
+                }
+
+            }
+            foreach (var art in artikel) {
+                foreach (var line in art) {
+                    Console.WriteLine(line);
+                }
+            }
+
         }
     }
 }
