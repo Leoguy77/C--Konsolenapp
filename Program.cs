2ff4b71d57ea7e357054ec2c7c5b6459e389e9f4
leon.schneider <leonschneider29@googlemail.com>
2025-06-12 14:57:07 +0200

    .

--- Program.cs
+++ Program.cs
@@ -13,10 +13,12 @@ namespace C__Konsolenapp {
             string file = args[0].ToString();
             if (File.Exists(file)) {
                 String[] lines = File.ReadAllLines(file);
-                for (int i = 0; i < lines.Length; i++) {
-                    if (i == 1) continue;
+                for (int i = 1; i < lines.Length; i++) {
                     string line = lines[i];
-                    artikel.Add(line.Split(";"));
+                    String[] values = line.Split(";");
+                    values[2].Replace(".", ",");
+                    values[3].Replace(".", ",");
+                    artikel.Add(values);
                 }
 
             }
@@ -26,13 +28,24 @@ namespace C__Konsolenapp {
                 }
             }
 
+
+
             OracleConnectionStringBuilder connBuilder = new OracleConnectionStringBuilder();
             connBuilder.DataSource = String.Format("{0}:{1}/{2}", db_host, db_port, db_srvname);
             connBuilder.UserID = db_username;
             connBuilder.Password = db_password;
             using (Oracle.ManagedDataAccess.Client.OracleConnection connection = new(connBuilder.ConnectionString)) {
                 connection.Open();
-                // Do something
+                foreach (String[] art in artikel) {
+                    var command = new OracleCommand($"INSERT INTO Artikel(ARTNR,ARTBEZEICHNUNG,MWST,ARTPREIS) VALUES (:ARNT,:ARTBEZEICHNUNG,:MWST,:ARTPREIS)", connection);
+                    command.Parameters.Add("ARNT", art[0]);
+                    command.Parameters.Add("ARTBEZEICHNUNG", art[1]);
+                    command.Parameters.Add("MWST", (Double.Parse(art[2])));
+                    command.Parameters.Add("ARTPREIS", (Double.Parse(art[3])));
+                    command.ExecuteNonQuery();
+                }
+
+
                 connection.Close();
             }
         }
